039cbbbd47daff0fca5eeaeaf1667fcb1a826126
Bashar709 <186461@stud.hvl.no>
2024-10-25 10:21:20 +0200

    begynt på 3

--- src/main/java/no/hvl/dat100/oppgave3/Blogg.java
+++ src/main/java/no/hvl/dat100/oppgave3/Blogg.java
@@ -4,29 +4,35 @@ import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave1.*;
 
 public class Blogg {
-
-	// TODO: objektvariable 
+	private Innlegg[] tabell;
+	private int nesteLedige;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		tabell = new Innlegg[20];
+		nesteLedige = 0;
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		tabell = new Innlegg[lengde];
+		nesteLedige = 0;
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteLedige;
 	}
-	
+
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return tabell;
 
 	}
-	
-	public int finnInnlegg(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+	public int finnInnlegg(Innlegg innlegg) {
+		int pos = finnInnlegg(innlegg);
+		if (pos >= 0) { // funnet!
+			return tabell[pos];
+		} else {
+			return null;
+		}
 	}
 
 	public boolean finnes(Innlegg innlegg) {
@@ -37,35 +43,35 @@ public class Blogg {
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
-	
+
 	public boolean leggTil(Innlegg innlegg) {
 
 		throw new UnsupportedOperationException(TODO.method());
 	}
-	
+
 	public String toString() {
 		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	// valgfrie oppgaver nedenfor
-	
+
 	public void utvid() {
 		throw new UnsupportedOperationException(TODO.method());
 	}
-	
+
 	public boolean leggTilUtvid(Innlegg innlegg) {
 
 		throw new UnsupportedOperationException(TODO.method());
-		
+
 	}
-	
+
 	public boolean slett(Innlegg innlegg) {
-		
+
 		throw new UnsupportedOperationException(TODO.method());
 	}
-	
+
 	public int[] search(String keyword) {
-		
+
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
